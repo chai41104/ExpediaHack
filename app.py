0e7035dc22d03afa2d19c1e724bf792c2a5c3578
chaitype <chai41104@hotmail.com>
2018-12-08 03:34:59 +0000

    passing data from front to back and return back

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 
 from flask import Flask
+from flask import request
+
 app = Flask(__name__, static_url_path='', static_folder='public')
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/query", methods = ['POST'])
+def query():
+    print (request.is_json)
+    content = request.get_json()
+    print (content)
+    return 'JSON posted'
